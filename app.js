d7d41fafd31c1b3a4227fc4bf3f7fd4b6f38e7a5
marine112 <patrickoliver322@gmail.com>
2023-10-11 09:09:51 -0300

    added my first files to github

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+
+let assingment = "c"
+console.log("assingment");
+
+let a = 10
+let b = 5
+
+let x = 50
+let z = 50
+
+
+// Addition +
+console.log(a + b);         
+
+console.log(x + z);
+ let k = 25
+ // subtraction-
+ console.log(x + z - k);
+ // multiplication *
+ console.log((x + z) * 3);
+
+
+
+ letprice = 20
+ let numbergoods = 5
+ // multiplication *
+ console.log(20 * 5);
+ totalcost = "20 * 5"
+
+ let d = 50
+ // multiplication*
+ console.log(d * 0.1);
+
+ let num1= 30
+ let num2= 40
+ // division /
+ console.log(30/40);
+
---
+++ condition.js
@@ -0,0 +1,31 @@
+// if ( 30 < 30 ){
+//     console.log("30 is greater");
+// } else if(35 < 3){
+//     console.log("35 is greater");
+// }else{
+//     console.log("no condition passed")
+
+// }
+// let v= 50
+// let x= 20
+
+// if(x > v){
+//     console.log(x+" is greater");
+// }else if(v > x){
+//     console.log(v+" is greater");
+// }else{
+//     console.log("both are equal");
+// }
+
+// AND &&
+// OR ||
+// NOT !
+
+let apple = 5  
+let fish = 6
+
+if(apple != 2 && fish != 5){
+    console.log("false");
+}else{
+    console.log("true");
+}
